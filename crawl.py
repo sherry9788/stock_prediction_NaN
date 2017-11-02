04b13a182b994be3fbd596c789a4ddd0d625871e
Pei Zhou <zpus223@126.com>
2017-11-01 21:22:28 -0700

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -2,7 +2,7 @@ import tweepy
 from tweepy import OAuthHandler
  
 consumer_key = 'O2cRYY5H68dpd416mkAMfyBaH'
-consumer_secret = '	AlyMBZtoZyjDluuP5LN464zMIdj3k28Ccmr6G5Nqkvu2p7RvCv'
+consumer_secret = 'AlyMBZtoZyjDluuP5LN464zMIdj3k28Ccmr6G5Nqkvu2p7RvCv'
 access_token = '345850017-TjvWKWQEDZNxTkiu8JJ8yCvD4zAY7oAJW2v4biWA'
 access_secret = 'w98rJiftZBPcaRbmZx7YKElnd8R2MjMKvgN8MqktXtfWX'
 
