5975234ffd3154ee5fe3b9fe0dc1fe549db80828
Shuoyi Wei <33303673+weishuoyi@users.noreply.github.com>
2017-11-04 14:25:06 -0700

    Update and rename crawl.py to crawler.py
    Added code for streaming tweets including key word "iphone X"

--- crawler.py
+++ crawler.py
@@ -1,6 +1,6 @@
 import tweepy
 from tweepy import OAuthHandler
- 
+
 consumer_key = 'O2cRYY5H68dpd416mkAMfyBaH'
 consumer_secret = 'AlyMBZtoZyjDluuP5LN464zMIdj3k28Ccmr6G5Nqkvu2p7RvCv'
 access_token = '345850017-TjvWKWQEDZNxTkiu8JJ8yCvD4zAY7oAJW2v4biWA'
@@ -8,8 +8,10 @@ access_secret = 'w98rJiftZBPcaRbmZx7YKElnd8R2MjMKvgN8MqktXtfWX'
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
- 
 api = tweepy.API(auth)
-
-
+results = api.search(q="iphone x", )
+for r in results:
+    print "--------------------------------------------------------------"
+    print r.text
+print "--------------------------------------------------------------"
 
