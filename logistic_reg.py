6831ba47d16fef09551539e992f0ced404b78f98
sherry9788 <sherry9788@g.ucla.edu>
2017-12-09 22:14:58 -0800

    models

--- logistic_reg.py
+++ logistic_reg.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
 from sklearn.cross_validation import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 
 df = pd.read_csv('11.29_vectors.csv', header=0)
 
@@ -9,10 +9,10 @@ X = df.iloc[:, :299]
 y = df.iloc[:, 300]
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.33)
 
-clf = RandomForestClassifier(max_depth=2, random_state=0)
-
+clf = LogisticRegression()
 clf.fit(X_train,y_train)
 res = clf.predict(X_test)
 print ('score Scikit learn: ', clf.score(X_test, y_test))
 
 np.savetxt('result_11-29.csv', res, delimiter=',') 
+
