d4b842e4821b1938161a5831ba1a3cacc8652559
sherry9788 <sherry9788@g.ucla.edu>
2017-12-10 15:05:23 -0800

    try merge

--- random_forest.py
+++ random_forest.py
@@ -1,20 +1,126 @@
 import numpy as np
 import pandas as pd
 import glob
-from sklearn.cross_validation import train_test_split
+from sklearn.metrics import roc_curve, auc
+from sklearn import preprocessing 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import train_test_split
+from sklearn.multiclass import OneVsRestClassifier
+import matplotlib.pyplot as plt
+from scipy import interp
+from itertools import cycle
 
-all_files = glob.glob("vectors/*.csv")
-df = pd.concat((pd.read_csv(f, header = None) for f in all_files), axis=0)
+# files in /vectors
+#all_files = glob.glob("vectors/*.csv")
+#df = pd.DataFrame()
+#list_ = []
+#for file_ in all_files:
+#    frame = pd.read_csv(file_,index_col=None, header=None)
+#    list_.append(frame)
+#df = pd.concat(list_, axis = 0)
+
+#df = pd.concat((pd.read_csv(f, header = None) for f in all_files), axis=0)
+
+df = pd.read_csv("vectors/new_vectors_with_labels.csv")
 
 X = df.iloc[:, :299]
 y = df.iloc[:, 300]
-X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.1)
 
-clf = RandomForestClassifier(max_depth=2, random_state=0)
+# Binarize the output
+lb = preprocessing.LabelBinarizer()
+y = lb.fit_transform(y)
+n_classes = y.shape[1]
 
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.33)
+
+clf = OneVsRestClassifier(RandomForestClassifier())
 clf.fit(X_train,y_train)
 res = clf.predict(X_test)
 print ('score Scikit learn: ', clf.score(X_test, y_test))
+y_score = clf.predict_proba(X_test)
 
-np.savetxt('result_11-29.csv', res, delimiter=',') 
+# Compute ROC curve and ROC area for each class
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(n_classes):
+    fpr[i], tpr[i], _ = roc_curve(y_test[:, i], y_score[:, i])
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+# Compute micro-average ROC curve and ROC area
+fpr["micro"], tpr["micro"], _ = roc_curve(y_test.ravel(), y_score.ravel())
+roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+# Compute macro-average ROC curve and ROC area
+
+# First aggregate all false positive rates
+all_fpr = np.unique(np.concatenate([fpr[i] for i in range(n_classes)]))
+
+# Then interpolate all ROC curves at this points
+mean_tpr = np.zeros_like(all_fpr)
+for i in range(n_classes):
+    mean_tpr += interp(all_fpr, fpr[i], tpr[i])
+
+# Finally average it and compute AUC
+mean_tpr /= n_classes
+
+fpr["macro"] = all_fpr
+tpr["macro"] = mean_tpr
+roc_auc["macro"] = auc(fpr["macro"], tpr["macro"])
+
+# Plot all ROC curves
+fig = plt.figure()
+lw = 2
+plt.plot(fpr["micro"], tpr["micro"],
+         label='micro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["micro"]),
+         color='deeppink', linestyle=':', linewidth=4)
+
+plt.plot(fpr["macro"], tpr["macro"],
+         label='macro-average ROC curve (area = {0:0.2f})'
+               ''.format(roc_auc["macro"]),
+         color='navy', linestyle=':', linewidth=4)
+
+colors = cycle(['aqua', 'darkorange', 'cornflowerblue'])
+for i, color in zip(range(n_classes), colors):
+    plt.plot(fpr[i], tpr[i], color=color, lw=lw,
+             label='ROC curve of class {0} (area = {1:0.2f})'
+             ''.format(i+1, roc_auc[i]))
+
+plt.plot([0, 1], [0, 1], 'k--', lw=lw)
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Some extension of Receiver operating characteristic to multi-class')
+plt.legend(loc="lower right")
+plt.show()
+fig.savefig('roc_rand.png')
+
+#i = 0
+#for vec in list_:
+#    X = vec.iloc[:, :299]
+#    y = vec.iloc[:, 300]
+#    pred = clf.predict(X)
+#    pred = lb.inverse_transform(pred)
+#    np.savetxt('result_log/result'+str(i)+'.csv', pred, fmt='%10.5f', delimiter=',') 
+#    i += 1 
+
+
+# new
+i = 0
+for i in range(8):
+    X = df.iloc[i*100:(i+1)*100-1, :299]
+    y = df.iloc[i*100:(i+1)*100-1, 300]
+    pred = clf.predict(X)
+    pred = lb.inverse_transform(pred)
+    np.savetxt('result_rand/result'+str(i)+'.csv', pred, fmt='%10.5f', delimiter=',') 
+    
+i = 8
+X = df.iloc[i*100:, :299]
+y = df.iloc[i*100:, 300]
+pred = clf.predict(X)
+pred = lb.inverse_transform(pred)
+np.savetxt('result_rand/result'+str(i)+'.csv', pred, fmt='%10.5f', delimiter=',') 
+
+
