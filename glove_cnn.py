1d64a775aa2a374a2673f06197be1783b76af091
Hao Wang <haowanglud@g.ucla.edu>
2017-12-10 23:22:23 -0800

    deleted useless header

--- glove_cnn.py
+++ glove_cnn.py
@@ -1,14 +1,3 @@
-'''This script loads pre-trained word embeddings (GloVe embeddings)
-into a frozen Keras Embedding layer, and uses it to
-train a text classification model on the 20 Newsgroup dataset
-(classification of newsgroup messages into 20 different categories).
-GloVe embedding data can be found at:
-http://nlp.stanford.edu/data/glove.6B.zip
-(source page: http://nlp.stanford.edu/projects/glove/)
-20 Newsgroup data can be found at:
-http://www.cs.cmu.edu/afs/cs.cmu.edu/project/theo-20/www/data/news20.html
-'''
-
 from __future__ import print_function
 
 import os
@@ -68,7 +57,7 @@ def predict(dir_path):
                 class_prob = model.predict(data)
                 class_category = []
                 for prob in class_prob:
-                    index = np.argmax(prob)
+                    index = np.argmax(prob)+1
                     output.write("{0}\n".format(index))
                     class_category.append(index)
                 print(class_category)
